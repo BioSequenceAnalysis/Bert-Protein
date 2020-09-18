ebbb74990707fdb59277524ecf286766257b2661
xiguashuiguo <xiguashuiguo@gmail.com>
2020-09-18 13:49:43 +0800

    shell更新

--- run_fine_tune.sh
+++ run_fine_tune.sh
@@ -1,4 +1,13 @@
 # 设置可以使用的GPU
-# export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=1
+NUM = '1'
+DATANAME = "all_data"
 python ./ljy_run_classifier.py/
-  --dataname=all_data
+  --dataname=$DATANAME /
+  --do_eval=True /
+  --learning_rate=2e-6 /
+  --data_root='./dataset/${NUM}kmer_tfrecord/${DATANAME}/' /
+  --vocab_file='./vocab/vocab_${NUM}kmer.txt' /
+  --init_checkpoint='./model/${NUM}kmer_model/model.ckpt' /
+  --bert_config='./bert_config_${NUM}.json' /
+  --save_path='./model/classifier_model_${NUM}kmer_${DATANAME}./model.ckpt'
