3f09340c961d0391f7fa0f66a0c1be752d3c1ccd
xiguashuiguo <xiguashuiguo@gmail.com>
2020-09-23 11:59:21 +0800

    a

--- run_fine_tune.sh
+++ run_fine_tune.sh
@@ -2,12 +2,12 @@
 export CUDA_VISIBLE_DEVICES=1
 NUM = '1'
 DATANAME = "all_data"
-python ./ljy_run_classifier.py/
-  --dataname=$DATANAME /
-  --do_eval=True /
-  --learning_rate=2e-6 /
-  --data_root='./dataset/${NUM}kmer_tfrecord/${DATANAME}/' /
-  --vocab_file='./vocab/vocab_${NUM}kmer.txt' /
-  --init_checkpoint='./model/${NUM}kmer_model/model.ckpt' /
-  --bert_config='./bert_config_${NUM}.json' /
+python ./ljy_run_classifier.py \
+  --dataname=$DATANAME \
+  --do_eval=True \
+  --learning_rate=2e-6 \
+  --data_root='./dataset/${NUM}kmer_tfrecord/${DATANAME}/' \
+  --vocab_file='./vocab/vocab_${NUM}kmer.txt' \
+  --init_checkpoint='./model/${NUM}kmer_model/model.ckpt' \
+  --bert_config='./bert_config_${NUM}.json' \
   --save_path='./model/classifier_model_${NUM}kmer_${DATANAME}./model.ckpt'
